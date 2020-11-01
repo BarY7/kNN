0dbbc97b2343f0fd10adc16026915e4cc0cb3a97
BarY7 <baryaacovi7@gmail.com>
2020-11-01 20:51:52 +0200

    Finished exercise

--- targil.py
+++ targil.py
@@ -1,31 +1,23 @@
-from matplotlib import pyplot
-from sklearn.datasets import fetch_openml
-import matplotlib.pyplot as plt
+from scipy.spatial import distance
 import numpy
-
-
-def compute_euclidean_comparator(image):  # image is 'raw' - no label attached.
-    def compute_relative_euclid(image2):
-        return compute_euclidean_distance(image, image2[0])
-    return compute_relative_euclid
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+import matplotlib
+matplotlib.use('TkAgg')
 
 
 def compute_euclidean_distance(image1, image2):
-    sum = 0
-    for i in range(0, len(image1)):
-        sum += (image1[i] - image2[i]) ** 2
-    return sum ** 0.5
+    return distance.euclidean(image1, image2)
 
-
+#(a)
 def predict(training_images, labels, query_image, k):
-    comparator = compute_euclidean_comparator(query_image)
-    images_with_labels = [(training_images[i], labels[i])
-                          for i in range(0, len(training_images))]
-    images_with_labels.sort(key=comparator)
+    images_distance = numpy.array([compute_euclidean_distance(
+        training_images[i], query_image) for i in range(len(training_images))])
+    lowest_k_indices = numpy.argpartition(images_distance, k)
     labels_countsort = numpy.array([0 for i in range(0, 10)])
     for i in range(0, k):
-        labels_countsort[int(images_with_labels[i][1])
-                         ] = labels_countsort[int(images_with_labels[i][1])] + 1
+        label = labels[lowest_k_indices[i]]
+        labels_countsort[int(label)] = labels_countsort[int(label)] + 1
     return labels_countsort.argmax()
 
 
@@ -38,10 +30,24 @@ train_labels = labels[idx[:10000]]
 test = data[idx[10000:], :].astype(int)
 test_labels = labels[idx[10000:]]
 
+#(b)
+first_1k_train = numpy.array(train[:1000])
+first_1k_labels = numpy.array(train_labels[:1000])
+k = 10
+error = 0
+for index in range(0, len(test)):
+    predict_label = predict(
+        first_1k_train, first_1k_labels, test[index], k)
+    if(predict_label != int(test_labels[index])):
+        error = error + 1
+positive_rate = 100 - error/len(test) * 100
+print("Positive rate for k=0 is {}".format(positive_rate))
+
+#(c)
 first_1k_train = numpy.array(train[:1000])
 first_1k_labels = numpy.array(train_labels[:1000])
 k_range = range(1, 101)
-error_vector = [0 for k in range(len(k_range))]
+positive_rate = [0 for k in range(len(k_range))]
 for k in k_range:
     error = 0
     for index in range(0, len(test)):
@@ -49,11 +55,30 @@ for k in k_range:
             first_1k_train, first_1k_labels, test[index], k)
         if(predict_label != int(test_labels[index])):
             error = error + 1
-    print("ERROR IS : {}".format(error/len(test) * 100))
-    error_vector[k-1] = error/len(test) * 100
-print("####################ERROR VECTOR#######################", error_vector)
-plt.plot(k_range, error_vector)
-plt.ylabel("errors")
+    positive_rate[k-1] = 100 - error/len(test) * 100
+plt.plot(k_range, positive_rate)
+plt.ylabel("positive %")
 plt.xlabel("k")
 plt.show()
-print("here")
+
+#(d)
+k = 1
+n_vector = range(100, 5100, 100)
+positive_rate = [0 for k in range(len(n_vector))]
+postive_rate_index = 0
+for n in n_vector:
+    train_n = numpy.array(train[:n])
+    labels_n = numpy.array(train_labels[:n])
+    error = 0
+    for index in range(0, len(test)):
+        predict_label = predict(
+            train_n, labels_n, test[index], k)
+        if(predict_label != int(test_labels[index])):
+            error = error + 1
+    positive_rate[postive_rate_index] = 100 - error/len(test) * 100
+    postive_rate_index = postive_rate_index + 1
+plt.plot(n_vector, positive_rate)
+plt.ylabel("positive %")
+plt.xlabel("number of training")
+plt.show()
+
